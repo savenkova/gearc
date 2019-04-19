9bcaf59d8fcb0c597aa4dc05aada1f43e528adcd
savenkova <gorka@maximail.org>
2019-04-19 17:55:23 +0500

    added jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    /*watch = require('gulp-watch'),*/
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade');
 
 sass.compiler = require('node-sass');
-
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-});
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/style.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css/style.css'))
+});
+
+gulp.task('jade', function() {
+    return gulp.src('app/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/style.scss', gulp.series('sass'));
+    gulp.watch('app/*.jade', gulp.series('jade'));
 });
